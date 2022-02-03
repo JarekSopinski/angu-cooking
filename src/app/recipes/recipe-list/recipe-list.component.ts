7134da6ab7989b18b7f12e7b59df70e6cc67be40
JarekSopinski <jarek.sopinski@gmail.com>
2022-02-03 17:48:19 +0100

    Managing Recipes in a Recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,15 +10,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes:Recipe[];
 
-  recipes:Recipe[] = [
-    new Recipe('A Recipe for Meat', 'How to make tasty meat', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-    new Recipe('A Recipe for Dumplings', 'How to make tasty dumplings', 'http://cdn.cnn.com/cnnnext/dam/assets/200811115525-04-best-polish-foods.jpg')
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService {
-    
+
+    private recipes:Recipe[] = [
+        new Recipe('A Recipe for Meat', 'How to make tasty meat', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+        new Recipe('A Recipe for Dumplings', 'How to make tasty dumplings', 'http://cdn.cnn.com/cnnnext/dam/assets/200811115525-04-best-polish-foods.jpg')
+    ];
+
+    getRecipes() {
+        return this.recipes.slice(); // slice to prevent direct access
+    }
+
 }
