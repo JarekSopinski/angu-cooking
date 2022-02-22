6f6074a8af3a1e29bbe0b970f40e14916dc85929
JarekSopinski <jarek.sopinski@gmail.com>
2022-02-22 16:46:35 +0100

    Adding Navigation to Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-header',
@@ -8,14 +8,9 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class HeaderComponent {
 
     collapsed:boolean = true;
-    @Output() featureSelected = new EventEmitter<string>();
 
     toggleNavbarCollapsed() {
         this.collapsed = !this.collapsed;
-    }
-
-    onSelect(feature:string) {
-        this.featureSelected.emit(feature);
     }
 
 }
