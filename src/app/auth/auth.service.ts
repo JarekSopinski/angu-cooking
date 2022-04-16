59330c6bcc799d9a61c4d63e79bedceda71737a3
JarekSopinski <jarek.sopinski@gmail.com>
2022-04-16 06:47:12 +0200

    Auto-Login with Local Storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -78,6 +78,31 @@ export class AuthService {
         this.router.navigate(['/auth']);
     }
 
+    autoLogin() {
+        const userData: {
+            email:string,
+            id:string,
+            _token:string,
+            _tokenExpirationDate:string
+        } = JSON.parse(localStorage.getItem('userData'));
+
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+        );
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+
+    }
+
     private handleAuthentication(email:string, userId:string, token:string, expiresIn:number) {
         /**
          * Both in login and signup process, we use data returned from API to create a new User and pass it to our Subject.
@@ -92,6 +117,7 @@ export class AuthService {
             expirationDate
         );
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes:HttpErrorResponse) {
