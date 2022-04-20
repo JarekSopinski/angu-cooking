68fdd41b0740b5f040b27f777b004d7fa74e69eb
JarekSopinski <jarek.sopinski@gmail.com>
2022-04-20 20:07:16 +0200

    Extracting feature module's routes

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -3,6 +3,7 @@ import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 
+import { RecipesRoutingModule } from "./recipes-routing.module";
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
@@ -22,7 +23,8 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
     imports: [
         RouterModule,
         CommonModule, // unlocks things like ngif, ngfor
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RecipesRoutingModule
     ],
     exports: [
         RecipesComponent,
