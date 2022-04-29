cb3925a594fa9588dc27e563052526e29c555e0e
JarekSopinski <jarek.sopinski@gmail.com>
2022-04-29 07:18:50 +0200

    Adding logic to the reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,3 +1,4 @@
+import { Action } from "@ngrx/store";
 import { Ingredient } from "../shared/ingredient.model";
 
 const initialState = {
@@ -10,6 +11,14 @@ const initialState = {
 
 }
 
-export function shoppingListReducer(state = initialState, action) {
+export function shoppingListReducer(state = initialState, action: Action) {
+
+    switch(action.type) {
+        case 'ADD_INGREDIENT':
+            return {
+                ...state,
+                ingredients: [...state.ingredients, action]
+            };
+    }
 
 }
