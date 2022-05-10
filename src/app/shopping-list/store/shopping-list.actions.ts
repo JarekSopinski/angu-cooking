1aa122e428459bc74c9151fb60651f0c1dc1362a
JarekSopinski <jarek.sopinski@gmail.com>
2022-05-10 06:06:04 +0200

    Prepering Update and Delete Actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from "src/app/shared/ingredient.model";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT'; // save action's name to variable to rule out manual typing
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action {
 
@@ -22,5 +24,27 @@ export class AddIngredients implements Action {
 
 }
 
+export class UpdateIngredient implements Action {
+
+    readonly type = UPDATE_INGREDIENT;
+    constructor(
+        public payload: {index: number, ingredient: Ingredient}
+    ) {}
+
+}
+
+export class DeleteIngredient implements Action {
+
+    readonly type = DELETE_INGREDIENT;
+    constructor(
+        public payload: number
+    ) {}
+
+}
+
 // Create new type for TS to recognize all our Actions
-export type ShoppingListActionsType = AddIngredient | AddIngredients;
+export type ShoppingListActionsType = 
+    | AddIngredient
+    | AddIngredients
+    | UpdateIngredient
+    | DeleteIngredient;
