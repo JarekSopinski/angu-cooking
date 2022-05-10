a982f532a61388721cfb19031d3aff8790f07db6
JarekSopinski <jarek.sopinski@gmail.com>
2022-05-10 07:28:10 +0200

    Cleaned up ShoppingListService - ShoppingList Redux done

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,7 +4,6 @@ import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list.service';
 import * as ShoppingListActions from '../store/shopping-list.actions';
 import * as fromShoppingList from '../store/shopping-list.reducer';
 
@@ -20,7 +19,6 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   editedItem:Ingredient;
 
   constructor(
-    private slService: ShoppingListService,
     private store: Store<fromShoppingList.AppState>
     ) { }
 
@@ -45,10 +43,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode){
-      // this.slService.updateIngredient(this.editedItemIndex, newIngredient);
       this.store.dispatch( new ShoppingListActions.UpdateIngredient(newIngredient) );
     } else {
-      // this.slService.addIngredient(newIngredient);
       this.store.dispatch( new ShoppingListActions.AddIngredient(newIngredient) );
     }
     this.editMode = false;
@@ -62,7 +58,6 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onDelete() {
-    // this.slService.deleteIngredient(this.editedItemIndex);
     this.store.dispatch(
       new ShoppingListActions.DeleteIngredient()
     );
