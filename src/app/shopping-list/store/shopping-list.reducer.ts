719fd7d41c2d8c958a02585115ab3f1aca51f386
JarekSopinski <jarek.sopinski@gmail.com>
2022-05-10 06:24:49 +0200

    Updating and Deleting Ingredients

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -17,16 +17,42 @@ export function shoppingListReducer(
     ) {
 
     switch(action.type) {
+
         case ShoppingListActions.ADD_INGREDIENT: // use variable, not string, to rule out manual typing
             return {
                 ...state, // include previous state
                 ingredients: [...state.ingredients, action.payload]
             };
+
         case ShoppingListActions.ADD_INGREDIENTS:
             return {
                 ...state,
                 ingredients: [...state.ingredients, ...action.payload]
             }
+
+        case ShoppingListActions.UPDATE_INGREDIENT:
+            const editedIngredient = state.ingredients[action.payload.index];
+            const updatedIngredient = {
+                ...editedIngredient,
+                ...action.payload.ingredient
+            };
+
+            const updatedIngredients = [...state.ingredients];
+            updatedIngredients[action.payload.index] = updatedIngredient;
+
+            return {
+                ...state,
+                ingredients: updatedIngredients
+            };
+
+        case ShoppingListActions.DELETE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: state.ingredients.filter((ig, igIndex) => {
+                    return igIndex !== action.payload;
+                })
+            };
+
         default:
             return state;
     }
