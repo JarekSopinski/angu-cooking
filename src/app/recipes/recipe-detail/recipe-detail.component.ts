a8334ce97bdc704b4ba482c05fd36917e6270ee5
JarekSopinski <jarek.sopinski@gmail.com>
2022-05-14 16:21:30 +0200

    Fetching Recipe Detail Data from Store

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { map, switchMap } from 'rxjs/operators';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -15,16 +20,29 @@ export class RecipeDetailComponent implements OnInit {
   constructor(
     private recipeService: RecipeService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private store: Store<fromApp.AppState>
     ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.id = +params['id'];
-        this.recipe = this.recipeService.getRecipe(this.id);
-      }
-    );
+
+    this.route.params.pipe(
+      // get recipe ID from route params observable
+      map(
+        params => { return +params['id'] }
+      ),
+      // switch observable from route params obs. to store obs.
+      switchMap(
+        id => { this.id = id; return this.store.select('recipes'); }
+      ),
+      // find current recipe in state
+      map(
+        recipesState => {
+          return recipesState.recipes.find( (recipe, index) => { return index === this.id } )
+        }
+      )
+    ).subscribe(recipe => { this.recipe = recipe });
+
   }
 
   onAddToShoppingList() {
