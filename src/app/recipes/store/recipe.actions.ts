f929e4a8379268718f628a43e60f6e67c3f3749a
JarekSopinski <jarek.sopinski@gmail.com>
2022-05-14 15:53:51 +0200

    Fetching Recipes with Store

---
+++ src/app/recipes/store/recipe.actions.ts
@@ -0,0 +1,17 @@
+import { Action } from "@ngrx/store";
+
+import { Recipe } from "../recipe.model";
+
+export const SET_RECIPES = '[Recipes] SET_RECIPES';
+
+export class SetRecipes implements Action {
+
+    readonly type = SET_RECIPES;
+
+    constructor(
+        public payload: Recipe[]
+    ) {}
+
+}
+
+export type RecipesActionsType = SetRecipes;
